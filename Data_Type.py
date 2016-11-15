83a1fccc08c74b12dfd2764e99283b508633cb1b
vineethmohanan <mail2vineethm@gmail.com>
2016-11-15 17:26:07 +0530

    Data_Type.py

---
+++ Data_Type.py
@@ -0,0 +1,8 @@
+i2 = int(raw_input()) # read int
+d2 = float(raw_input()) # read double 
+s2 = raw_input() # read string
+
+# print summed and concatenated values
+print(i + i2)
+print(d + d2)
+print(s + s2)
